cf63c7db01e5879ed42eddf85debb9479e3abcc1
yeqing <heopro@163.com>
2023-06-09 00:13:28 +0800

    push: Naive implementation

--- ugit/data.py
+++ ugit/data.py
@@ -110,3 +110,8 @@ def fetch_object_if_missing(oid, remote_git_dir):
         return
     remote_git_dir += "/.ugit"
     shutil.copy(f"{remote_git_dir}/objects/{oid}", f"{GIT_DIR}/objects/{oid}")
+
+
+def push_object(oid, remote_git_dir):
+    remote_git_dir += "/.ugit"
+    shutil.copy(f"{GIT_DIR}/objects/{oid}", f"{remote_git_dir}/objects/{oid}")
