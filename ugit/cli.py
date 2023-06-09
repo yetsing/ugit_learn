1fead7919799627424a1e399531e663bda58864e
yeqing <heopro@163.com>
2023-06-09 21:17:30 +0800

    status: Show staged and non-staged modified files

--- ugit/cli.py
+++ ugit/cli.py
@@ -235,7 +235,13 @@ def status(args: argparse.Namespace) -> None:
     print("\nChanges to be committed:\n")
     HEAD_tree = HEAD and base.get_commit(HEAD).tree
     for path, action in diff.iter_changed_files(
-        base.get_tree(HEAD_tree), base.get_working_tree()
+        base.get_tree(HEAD_tree), base.get_index_tree()
+    ):
+        print(f"{action:>12}: {path}")
+
+    print("\nChanges not staged for commit:\n")
+    for path, action in diff.iter_changed_files(
+        base.get_index_tree(), base.get_working_tree()
     ):
         print(f"{action:>12}: {path}")
 
